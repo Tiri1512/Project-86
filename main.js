da8f6104681e3683ada97e6bf4ecb30a1f668a69
Tiri1512 <83047448+Tiri1512@users.noreply.github.com>
2023-06-22 19:14:53 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 var canvas = new fabric.Canvas('myCanvas');
  var x= document.getElementById("myAudio");
 
-function new_image()
-{
-fabric.Image.fromURL('BirthdayImage.jpg' . function (Img))
+function new_image() {
+
+fabric.Image.fromURL('BirthdayImage.jpg', function (Img) {
 block_image_object = Img;
 
 block_image_object.scaleToWidth(700);
@@ -13,6 +13,8 @@ block_image_object.set({
   left:0
 });
 canvas.add(block_image_object);
+});
+
 }
 
 function playSound(){
